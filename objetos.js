6d7faabac906fc7183a63b469074d520b02137cf
matt <mnjprogramacion@gmail.com>
2025-05-20 17:25:08 +0200

    objetos

---
+++ objetos.js
@@ -0,0 +1,14 @@
+const house = {type:"flat", size:"40 m2", region:"uptown"};
+
+const house2 = {};
+house2.type = "house"; house2.size="320 m2"; house2.region="suburbs";
+
+function objeto1() {
+    let obj1 = "type = " + house.type + ", size = " + house.size + ", region = " + house.region;
+    return alert(obj1);
+}
+
+function objeto2() {
+    let obj2 = "type = " + house2.type + ", size = " + house2.size + ", region = " + house2.region;
+    return alert(obj2);
+}
