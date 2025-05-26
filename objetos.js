3aa5c0adcf6e749dbf9e9ccbef12343924278853
matt <mnjprogramacion@gmail.com>
2025-05-26 20:24:50 +0200

    bug

--- objetos.js
+++ objetos.js
@@ -6,9 +6,9 @@ house2.type = "house"; house2.size="320 m2"; house2.region="suburbs";
 function objeto1() {
     let obj1 = "type = " + house.type + ", size = " + house.size + ", region = " + house.region;
     return alert(obj1);
-}
+};
 
 function objeto2() {
     let obj2 = "type = " + house2.type + ", size = " + house2.size + ", region = " + house2.region;
     return alert(obj2);
-}
+};
